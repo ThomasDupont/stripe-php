6643899cd753b407d903b06d1a11783f0dd77fdb
Olivier Bellone <ob@stripe.com>
2018-11-16 10:47:58 +0100

    Update constructors of Stripe exception classes

--- lib/Error/OAuth/InvalidClient.php
+++ lib/Error/OAuth/InvalidClient.php
@@ -3,7 +3,11 @@
 namespace Stripe\Error\OAuth;
 
 /**
- * InvalidClient is raised when authentication fails.
+ * InvalidClient is raised when the client doesn't belong to you, or the API key mode (live or test)
+ * doesn't match the client mode. Or the stripe_user_id doesn't exist or isn't connected to your
+ * application.
+ *
+ * @package Stripe\Error\OAuth
  */
 class InvalidClient extends OAuthBase
 {
